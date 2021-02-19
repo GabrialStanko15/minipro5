91ae16c9dd52d41d7cb8bf45099a76e3e1aa0cbe
GabrialStanko15 <gabrial.stanko@patelhs.org>
2021-02-19 15:30:33 +0000

    method name and added proper countdown using recursion.

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 class Main {
   public static void main(String[] args) {
-   liftOff(1);
+   countdown(10);
 
 
     //ask if rocket is fueled and must reach certain requirment. if fuel is so an so than lift off. else abort mission. if strapped in launch else abort mission. if all are correcct then launch. 10 9 8 7 6 5 4 3 2 1 lift off!.mph if maxed out speed.
   }
-    public static void liftOff(int n) {
+    public static void countdown(int n) {
 
       if(n == 0) {
         System.out.println("Lift off! ");
       } else {
-        System.out.println("10 9 8 7 6 5 4 3 2 1 ");
+        System.out.println(n);
         n--;
-        liftOff(n);
+        countdown(n);
       }
      
     }
