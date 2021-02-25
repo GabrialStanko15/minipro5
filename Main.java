84995df06b2e3186153691e0831e4d776550c836
GabrialStanko15 <gabrial.stanko@patelhs.org>
2021-02-25 14:17:42 +0000

    changed code

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 class Main {
   public static void main(String[] args) {
-   countdown(10);
-
-
-    //ask if rocket is fueled and must reach certain requirment. if fuel is so an so than lift off. else abort mission. if strapped in launch else abort mission. if all are correcct then launch. 10 9 8 7 6 5 4 3 2 1 lift off!.mph if maxed out speed.
+    countdown(10);
   }
     public static void countdown(int n) {
 
@@ -14,6 +11,13 @@ class Main {
         n--;
         countdown(n);
       }
+  
      
-    }
+       
+       
+     
+
+     
+    
   }
+}
